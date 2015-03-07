dd30056dc9fb01a97ae51ebd94b09a83e020ec78
josephharveyangeles <josephharveyangeles@gmail.com>
2015-03-07 12:12:18 +0800

    Project setup. symlinked code dir to my project dir.

---
+++ src/com/creosoft/inventarium/LauncherView.java
@@ -0,0 +1,15 @@
+package com.creosoft.inventarium;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LauncherView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_launcher_view);
+	}
+}
